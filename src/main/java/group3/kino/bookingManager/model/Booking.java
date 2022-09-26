44ae0af7d39d4b5fee28d1425bedb036fabb0393
SkibstedN <niko933n@stud.kea.dk>
2022-09-26 12:48:25 +0200

    2 crud

--- src/main/java/group3/kino/bookingManager/model/Booking.java
+++ src/main/java/group3/kino/bookingManager/model/Booking.java
@@ -1,6 +1,21 @@
 package group3.kino.bookingManager.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private int seatNumber;
     private String movie;
