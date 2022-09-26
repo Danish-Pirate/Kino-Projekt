cdbb44ad48c6c7889f03247a65661f956adde641
Søs <slys87@gmail.com>
2022-09-26 15:54:53 +0200

    1. kommit

--- src/main/java/group3/kino/bookingManager/model/Booking.java
+++
@@ -1,7 +0,0 @@
-package group3.kino.bookingManager.model;
-
-public class Booking {
-    private Long id;
-    private int seatNumber;
-    private String movie;
-}
--- src/main/java/group3/kino/bookingManager/repository/BookingRepository.java
+++
@@ -1,4 +0,0 @@
-package group3.kino.bookingManager.repository;
-
-public interface BookingRepository {
-}
