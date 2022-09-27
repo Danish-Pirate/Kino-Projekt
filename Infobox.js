aa5971eca384e89720f519b4269f77e442ebcfbe
Baeist <lars18n2@stud.kea.dk>
2022-09-27 13:13:43 +0200

    initial for booking ui

---
+++ Infobox.js
@@ -0,0 +1,25 @@
+function addData(){
+
+    
+
+
+var cinemaName = "Biograf: "+ "Bio1";
+var movieName = "Filmnavn: " + "Dødets gab";
+var date = "Dato: " + "22/10/2022";
+var startTime = "Tidspunkt: " + "14:00"
+var ageRestriction = "Minimums alder: " + "13";
+var price = "Pris: " + "100" + "dkr.";
+
+    document.getElementById('cinema_name_infobox_id').textContent = cinemaName;
+
+    document.getElementById('movie_name_infobox_id').textContent = movieName;
+
+    document.getElementById('date_infobox_id').textContent = date;
+
+    document.getElementById('start_time_infobox_id').textContent = startTime;
+
+    document.getElementById('age_infobox_id').textContent = ageRestriction;
+
+    document.getElementById('price_infobox_id').textContent = price;
+
+}
